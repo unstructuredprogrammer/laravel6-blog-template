e1b5e51a769b773dd35e1ff16b71352583729a03
pppppppppppppppuuuuuurrrrrrreeeeeeee <adsadsdsd1@gmail.com>
2021-05-30 20:55:18 +0430

    finished

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/master')
+
+@section('title', 'root')
+@section('articleHeader', 'root')
+
+
+@section('sidebar')
+    @parent
+    <h1>Side Bar</h1>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>@yield('title')</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        @include('layouts/header')
+
+        <!-- Page content-->
+        <div class="container mt-5">
+            <div class="row">
+                <!-- Content-->
+                @include('layouts/article')
+
+                <!-- Side widgets-->
+                @section('sidebar')
+                    @include('layouts/sidebar')
+                @show
+            </div>
+        </div>
+        <!-- Footer-->
+        @include('layouts/footer')
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
